23f2e45aac375cebd5af5d04863f915f415233b1
Danilo Alves <danilovieiraalves@hotmail.com>
2022-07-05 20:22:34 -0300

    feat(cpf): Implements generate

--- src/commands/cpf.rs
+++ src/commands/cpf.rs
@@ -1,3 +1,4 @@
+use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
 
 use regex::Regex;
@@ -20,8 +21,59 @@ fn clean_mask(value: String) -> String {
     }
 }
 
+fn format_cpf(value: String) -> String {
+    let reg_exp = Regex::new(r"(\d{3})(\d{3})(\d{3})(\d{2})").unwrap();
+
+    reg_exp.replace(value.as_str(), "$1.$2.$3-$4").to_string()
+}
+
 pub fn generate() {
-    println!("Not implemented yet");
+    let mut rng = thread_rng();
+    let mut raw_cpf = String::new();
+
+    for _ in 0..9 {
+        let n: usize = rng.gen_range(0..=9);
+
+        raw_cpf.push_str(n.to_string().as_str());
+    }
+
+    let mut first_digit_sum = 0;
+    let mut val_chars = raw_cpf.graphemes(true).collect::<Vec<&str>>();
+
+    for i in 0..9 {
+        let num: usize = val_chars[i].parse().unwrap();
+
+        first_digit_sum += num * (10 - i);
+    }
+
+    let first_rest = first_digit_sum % 11;
+    let first_digit = match first_rest.cmp(&2) {
+        Ordering::Less => 0,
+        _ => 11 - first_rest,
+    };
+
+    let first_digit_string = first_digit.to_string();
+
+    val_chars.push(first_digit_string.as_str());
+
+    let mut second_digit_sum = 0;
+
+    for i in 0..10 {
+        let num: usize = val_chars[i].parse().unwrap();
+
+        second_digit_sum += num * (11 - i);
+    }
+
+    let second_digit_rest = second_digit_sum % 11;
+    let second_digit = match second_digit_rest.cmp(&2) {
+        Ordering::Less => 0,
+        _ => 11 - second_digit_rest,
+    };
+
+    let second_digit_string = second_digit.to_string();
+    val_chars.push(second_digit_string.as_str());
+
+    println!("{:?}", format_cpf(val_chars.join("")));
 }
 
 pub fn validate(value: &Option<String>) {
